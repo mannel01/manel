688fe80a69068745330f9d5435f2cb38167cb0f0
manel.br <otaviomanumito007@gmail.com>
2025-11-21 00:08:37 -0300

    refatoramento do código
    em andamento

--- .github/workflows/ML Games/Log/Login.js
+++ .github/workflows/ML Games/Log/Login.js
@@ -1,16 +1,3 @@
-const barra = document.getElementById("barra_admin");
-const seta = document.getElementById("seta_baixar");
-
-let aberta = false;
-
-seta.addEventListener("click", () => {
-    aberta = !aberta;
-
-    if (aberta) {
-        barra.style.top = "0px";
-        seta.style.transform = "rotate(180deg)";
-    } else {
-        barra.style.top = "-80px";
-        seta.style.transform = "rotate(0deg)";
-    }
-});
+const User = document.getElementById("User");
+const Pass = document.getElementById("Pass");
+const ButtonEntrar = document.getElementById("ButtonEntrar");
