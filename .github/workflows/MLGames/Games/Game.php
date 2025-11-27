cfb2a1f02a6008114f699f5f202df130adee8f13
Joseph <119811718+Polar658@users.noreply.github.com>
2025-11-27 08:42:52 -0300

    melhoria visual

--- .github/workflows/MLGames/Games/Game.php
+++ .github/workflows/MLGames/Games/Game.php
@@ -70,13 +70,17 @@ $result = mysqli_query($conexao, $sql_select);
 
         </form>
 
-        <!-- <div class="vidro2"></div> -->
-        <input type="search" name="pesquisar" placeholder="Pesquisar">
+        <div class="vidro4">
+            
+            <input id="p" type="search" name="pesquisar" placeholder="Pesquisar">
         <button>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
             </svg>
         </button>
+        </div>
+        <!-- <div class="vidro2"></div> -->
+        
 
         <div class="vidro3">
             <table class="table">
