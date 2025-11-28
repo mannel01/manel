5eb864af4da9c0fd97bfb679b8a375cb9b3b25cd
Joseph <119811718+Polar658@users.noreply.github.com>
2025-11-28 07:24:51 -0300

    jjfilmes

--- JJFilmes/Home/Home.php
+++ JJFilmes/Home/Home.php
@@ -31,7 +31,7 @@
 
 
         <div class="vidro2">
-            <img src="../Imgs/ML Games.png" alt="">
+            <img src="../Imgs/" alt="">
         </div>
         <div class="btn-av">
             <a href="Sair.php"><button class="btn-avo">Sair</button></a>
